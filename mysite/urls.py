f223b77cb52949f5c3a4327ac64d8bcbee1b6b93
jindasy <jindaporn.sy@gmail.com>
2022-09-12 17:41:23 +0700

    remove unused import

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,10 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import redirect
 from django.views.generic.base import RedirectView
-
-from polls import views
 
 
 urlpatterns = [

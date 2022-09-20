a53fcb4b64daf111b42cc9a54cc1060ed065ad59
jindasy <jindaporn.sy@gmail.com>
2022-09-20 21:08:02 +0700

    edit failed test

--- polls/tests.py
+++ polls/tests.py
@@ -153,7 +153,7 @@ class QuestionDetailViewTests(TestCase):
     def test_future_question(self):
         """
         The detail view of a question with a pub_date in the future
-        returns a 404 not found.
+        redirect to index page.
         """
         future_question = create_question(question_text='Future question.', days=5)
         url = reverse('polls:detail', args=(future_question.id,))
@@ -163,13 +163,13 @@ class QuestionDetailViewTests(TestCase):
     def test_past_question(self):
         """
         The detail view of a question with a pub_date in the past
-        displays the question's text.
+        redirect to detail view.
         """
         past_question = create_question(question_text='Past Question.', days=-5)
         url = reverse('polls:detail', args=(past_question.id,))
         response = self.client.get(url)
-        self.assertContains(response, past_question.question_text)
-        # self.assertEqual(response.status_code, 302)
+        # self.assertContains(response, past_question.question_text)
+        self.assertEqual(response.status_code, 302)
 
 
 class AuthenticateUserTest(TestCase):
